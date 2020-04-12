258749f18bbacff98c9649501c8bce727e749e22
Illia Grybkov <illia@grybkov.com>
2020-04-11 20:26:43 -0500

    👨‍🎨 Add DbRecreate command

--- src/Command/DbCreate.php
+++ src/Command/DbCreate.php
@@ -5,10 +5,10 @@ namespace App\Command;
 use App\CommandConfigurator\ConfigurableCommandInterface;
 use App\CommandConfigurator\DatabaseName;
 use App\Environment\Database;
+use App\Service\DatabaseManagement;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Process\Process;
 
 class DbCreate extends Command implements ConfigurableCommandInterface
 {
@@ -16,11 +16,16 @@ class DbCreate extends Command implements ConfigurableCommandInterface
      * @var Database
      */
     private $database;
+    /**
+     * @var DatabaseManagement
+     */
+    private $databaseManagement;
 
-    public function __construct(Database $database)
+    public function __construct(Database $database, DatabaseManagement $databaseManagement)
     {
         parent::__construct(null);
         $this->database = $database;
+        $this->databaseManagement = $databaseManagement;
     }
 
     protected function configure(): void
@@ -31,13 +36,13 @@ class DbCreate extends Command implements ConfigurableCommandInterface
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $process = new Process(['mysql']);
-        $process->setInput(sprintf("create database if not exists`%s`;", $this->database->getDatabase()));
-        $process->run();
-
-        $exitCode = (int)$process->getExitCode();
-        $output->writeln('Query complete. Exit code ' . $exitCode);
-        return $exitCode;
+        $createError = $this->databaseManagement->create($this->database->getDatabase());
+        if (null !== $createError) {
+            $output->writeln(sprintf("<error>Cannot create database: %s</error>", $createError));
+            return 1;
+        }
+        $output->writeln('Database created.');
+        return 0;
     }
 
     public function getConfiguratorsList(): array
